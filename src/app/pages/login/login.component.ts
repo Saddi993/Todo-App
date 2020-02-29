c663e94b297d8d029b9060dcff63ba491c7f932c
Saad Ahmed <saad.ahmed@linkagoal.com>
2020-02-29 23:23:43 +0500

    Login Page UI with validations

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	form: FormGroup;
 
-  ngOnInit() {
-  }
+	constructor(private fb: FormBuilder) { }
+
+	ngOnInit() {
+
+		this.form = this.fb.group({
+			username: ['', Validators.required],
+			password: ['', Validators.required]
+		});
+	}
+
+	onSubmit() {
+		const username = this.form.get('username').value;
+		const password = this.form.get('password').value;
+	}
 
 }
