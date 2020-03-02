cc94884c84b15aa99851afc339bb808f73af89dd
Saad Ahmed <saad.ahmed@linkagoal.com>
2020-03-02 16:40:22 +0500

    added todo rest service

---
+++ angular-src/src/app/providers/backend/todo.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { BackEndService } from './backend.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TodoRestService {
+
+  constructor(private backend: BackEndService) { }
+
+  getTaskList() {
+    return this.backend.get(`/tasks`);
+  }
+
+  createTask() {
+    return this.backend.post(`/tasks`, {});
+  }
+
+  updateTask(id: any) {
+    return this.backend.put(`/tasks/${id}`, {});
+  }
+}
